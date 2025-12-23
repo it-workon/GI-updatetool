ada8079f38a50e0cdc107d258f2a26127198872a
Eduardo Luís de Andrade Santos <eduardo775445@gmail.com>
2025-12-23 15:52:58 -0300

    fix: syntax error from ginfor.bat

--- ginfor.bat
+++ ginfor.bat
@@ -2,7 +2,7 @@
 echo Starting GI update...
 echo.
 
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0UpdateGI.ps1"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Updater.ps1"
 
 echo.
 echo Script finished.
